731990daed57bbd09fc21dcf73edec21b246a82c
Sheshank9763 <143405773+Sheshank9763@users.noreply.github.com>
2024-02-05 17:56:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = var.region
-    access_key = "AKIAT3CY7WA2WRKYMECQ"
-    secret_key = "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"
+    access_key = var.AWS_ACCESS_KEY_ID
+    secret_key = var.AWS_SECRET_ACCESS_KEY
 }
