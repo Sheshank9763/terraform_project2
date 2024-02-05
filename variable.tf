7bf22dd49156330b5fbf0d6a37465e9333650687
Sheshank9763 <143405773+Sheshank9763@users.noreply.github.com>
2024-02-05 19:41:05 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,10 +4,4 @@ variable "region" {
 variable "ami_id" {
     default = "ami-0ecb0bb5d6b19457a"
 }
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
-}
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Access Key"
-}
